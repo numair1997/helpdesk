5f21a2159efbc7ba4e5f8dfa011bc167aca3866f
numair1997 <152866939+numair1997@users.noreply.github.com>
2024-06-26 12:24:27 +0500

    saad commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "57a1429c639dd5a6"; }
+/******/ 		__webpack_require__.h = function() { return "a78c347ef4396262"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/home-page-components/subscription-plan.tsx
+++ components/home-page-components/subscription-plan.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+"use client"
+
+import Image from "next/image";
+import blogImg1 from "@/public/images/blog-post-image-1.png";
 
 const SubscriptionPlan = () => {
   return (
@@ -9,6 +12,32 @@ const SubscriptionPlan = () => {
           <p className="text-xl font-bold">Subscription Plan</p>
           <h2 className="text-4xl font-bold pt-5">Access Expert Assistance</h2>
         </div>
+        <div className="stp-15 grid grid-cols-12 gap-6">
+            <div className={`group col-span-12 sm:col-span-6 lg:col-span-4`}>
+              <div className="relative">
+                <Image src={blogImg1} alt="" className="w-full rounded-xl" />
+              </div>
+              <div className="flex flex-col items-center justify-center pt-10">
+                <h4 className="heading-4 pb-3">How To Register on this Platform?</h4>
+              </div>
+            </div>
+            <div className={`group col-span-12 sm:col-span-6 lg:col-span-4`}>
+              <div className="relative">
+                <Image src={blogImg1} alt="" className="w-full rounded-xl" />
+              </div>
+              <div className="flex flex-col items-center justify-center pt-10">
+                <h4 className="heading-4 pb-3">How To Find Helpers on this Platform?</h4>
+              </div>
+            </div>
+            <div className={`group col-span-12 sm:col-span-6 lg:col-span-4`}>
+              <div className="relative">
+                <Image src={blogImg1} alt="" className="w-full rounded-xl" />
+              </div>
+              <div className="flex flex-col items-center justify-center pt-10">
+                <h4 className="heading-4 pb-3">How To Make Payments on this Platform?</h4>
+              </div>
+            </div>
+          </div>
         <div className="flex items-center justify-center mt-10">
           <button className="mt-3 flex w-48 items-center justify-center gap-2 rounded-3xl border border-n30 py-3 bg-black text-white">
             <span className="text-xl font-bold">View All</span>
